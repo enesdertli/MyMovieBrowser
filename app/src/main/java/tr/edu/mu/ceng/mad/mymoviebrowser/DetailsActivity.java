edf767a3fb0c94ff9f214036432d584766fc0baf
U180709012 <enesdertli@posta.mu.edu.tr>
2021-11-14 03:24:48 +0300

    Initial commit

---
+++ app/src/main/java/tr/edu/mu/ceng/mad/mymoviebrowser/DetailsActivity.java
@@ -0,0 +1,21 @@
+package tr.edu.mu.ceng.mad.mymoviebrowser;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        Movie movie = getIntent().getParcelableExtra("movie");
+
+        FragmentTransaction fts = getSupportFragmentManager().beginTransaction();
+        fts.add(R.id.container, DetailsFragment.newInstance(movie));
+        fts.commit();
+    }
+}
